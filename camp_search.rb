2a6d5dc0cc0b8ba22332760392418a766b1bfd39
Sufyan Adam <pair+sufyanadam@gmail.com>
2015-04-04 20:48:27 -0700

    Check availability of all camp sites at sam p taylor park
    Check availability for the next 48 weekends only at sam p taylor for now.
    
    Print out status updates
    Print out results if there are any availabilities

--- camp_search.rb
+++ camp_search.rb
@@ -1,10 +1,10 @@
 require 'date'
 require 'open-uri'
-require 'nokogiri'
 require 'capybara'
 
 CAMPSITE_URLS = {
-  samuel_p_taylor: ->(date) { "http://www.reserveamerica.com/camping/Samuel_P_Taylor_Sp/r/campsiteDetails.do?arvdate=#{date.strftime('%m/%d/%Y')}&contractCode=CA&parkId=120081&siteId=3053" }
+  samuel_p_taylor: ->(date) { "http://www.reserveamerica.com/campsiteCalendar.do?page=matrix&calarvdate=#{date.strftime('%m/%d/%Y')}&contractCode=CA&parkId=120081"
+  }
 }
 
 def find_available_weekends(campsite_url, preferred_day: :friday, length_of_stay_in_nights: 2)
@@ -13,17 +13,37 @@ def find_available_weekends(campsite_url, preferred_day: :friday, length_of_stay
   available_weekends = []
 
   weekends.each do |day|
-  # day = weekends.first
     url = CAMPSITE_URLS[:samuel_p_taylor].(day)
     page = Capybara::Node::Simple.new(Nokogiri::HTML(open(url)))
-    availability = []
-    availability << page.find('#avail1')[:title]
+    availability = {}
+    availability[day] = {}
+    campsite_availability_rows = page.all('#calendar.items tbody tr')
+    campsite_availability_rows.each do |row|
+      next unless row.has_selector?('td.sn') && row.has_selector?('img') && /tent/.match(row.find('img')[:src])
 
-    length_of_stay_in_nights.times do |i|
-      availability << page.find("#avail#{i + 2}")[:title]
+      campsite_name = row.find('td.sn').text
+      campsite_link = row.find('td.sn').find('.siteListLabel a')[:href]
+      all_site_status_rows = row.all('td.status')
+
+      availability[day][campsite_name] = []
+
+      availability[day][campsite_name] << all_site_status_rows[0].text
+
+      (length_of_stay_in_nights - 1).times do |i|
+        availability[day][campsite_name] << all_site_status_rows[i + 1].text
+      end
+
+      if availability[day][campsite_name].all? { |status| status == 'A' }
+        available_weekends << {
+          date: day.strftime('%a %d %b %Y'),
+          campsite_name: campsite_name,
+          campsite_link: campsite_link,
+          length_of_stay: length_of_stay_in_nights
+        }
+      end
+
+      puts "Checking availability on #{day.strftime('%a %d %b %Y')}: site: #{campsite_name.gsub(/\s/, '')}, status: #{availability[day][campsite_name]}"
     end
-
-    available_weekends << day if availability.all? { |status| p 'the status!', status; status == 'Available' }
   end
 
   available_weekends
@@ -32,13 +52,15 @@ end
 def get_weekends(today, preferred_day)
   weekends = []
 
-  today.upto(today + 365).each do |day|
+  today.next.upto(today.next + 365).each do |day|
     weekends << day if day.send("#{preferred_day}?")
 
-    break if weekends.length >= 40
+    break if weekends.length >= 48
   end
 
   weekends
 end
 
-p find_available_weekends(nil)
+find_available_weekends(nil).each do |result_hash|
+  puts "Campsite #{result_hash[:campsite_name].gsub(/\s/, '')} is available on #{result_hash[:date]} for #{result_hash[:length_of_stay]} nights! Book it now at #{result_hash[:campsite_link]}"
+end
