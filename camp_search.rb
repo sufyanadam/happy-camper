68f5d02ba0387f88d1ff5fb229485050e4c0e5bf
Sufyan Adam <pair+sufyanadam@gmail.com>
2015-04-06 00:22:57 -0700

    Refactor CAMPSITE_URLS in favour of DRYing it out
    Also supports user entering in a specific site id to check availability
    for. Otherwise check all campsites at the park.

--- camp_search.rb
+++ camp_search.rb
@@ -1,21 +1,24 @@
 require 'date'
 require 'open-uri'
 require 'capybara'
-require 'pry'
 
-CAMPSITE_URLS = {
-  samuel_p_taylor:      ->(date) { "http://www.reserveamerica.com/campsiteCalendar.do?page=matrix&calarvdate=#{date.strftime('%m/%d/%Y')}&contractCode=CA&parkId=120081"  },
-  kirby_cove:           ->(date) { "http://www.reserveamerica.com/campsiteCalendar.do?page=matrix&calarvdate=#{date.strftime('%m/%d/%Y')}&contractCode=NRSO&parkId=70972" },
-  mt_tamalpais_sp:      ->(date) { "http://www.reserveamerica.com/campsiteCalendar.do?page=matrix&calarvdate=#{date.strftime('%m/%d/%Y')}&contractCode=CA&parkId=120063"  },
-  anthony_chabot:       ->(date) { "http://www.reserveamerica.com/campsiteCalendar.do?page=matrix&calarvdate=#{date.strftime('%m/%d/%Y')}&contractCode=EB&parkId=110004"  },
-  mt_diablo:            ->(date) { "http://www.reserveamerica.com/campsiteCalendar.do?page=matrix&calarvdate=#{date.strftime('%m/%d/%Y')}&contractCode=CA&parkId=120061"  },
-  sugarloaf_ridge:      ->(date) { "http://www.reserveamerica.com/campsiteCalendar.do?page=matrix&calarvdate=#{date.strftime('%m/%d/%Y')}&contractCode=CA&parkId=120092"  },
-  half_moon_bay:        ->(date) { "http://www.reserveamerica.com/campsiteCalendar.do?page=matrix&calarvdate=#{date.strftime('%m/%d/%Y')}&contractCode=CA&parkId=120039"  },
-  del_valle:            ->(date) { "http://www.reserveamerica.com/campsiteCalendar.do?page=matrix&calarvdate=#{date.strftime('%m/%d/%Y')}&contractCode=EB&parkId=110003"  },
-  bothenapa_valley_sp:  ->(date) { "http://www.reserveamerica.com/campsiteCalendar.do?page=matrix&calarvdate=#{date.strftime('%m/%d/%Y')}&contractCode=CA&parkId=120011"  },
-  portola_redwoods_sp:  ->(date) { "http://www.reserveamerica.com/campsiteCalendar.do?page=matrix&calarvdate=#{date.strftime('%m/%d/%Y')}&contractCode=CA&parkId=120073"  },
-  butano_sp:            ->(date) { "http://www.reserveamerica.com/campsiteCalendar.do?page=matrix&calarvdate=#{date.strftime('%m/%d/%Y')}&contractCode=CA&parkId=120013"  }
+PARKS = {
+  samuel_p_taylor_sp:  {contract_code: 'CA',   park_id: '120081' },
+  kirby_cove:          {contract_code: 'NRSO', park_id: '70972'  },
+  mt_tamalpais_sp:     {contract_code: 'CA',   park_id: '120063' },
+  anthony_chabot:      {contract_code: 'EB',   park_id: '110004' },
+  mt_diablo:           {contract_code: 'CA',   park_id: '120061' },
+  sugarloaf_ridge:     {contract_code: 'CA',   park_id: '120092' },
+  half_moon_bay:       {contract_code: 'CA',   park_id: '120039' },
+  del_valle:           {contract_code: 'EB',   park_id: '110003' },
+  bothenapa_valley_sp: {contract_code: 'CA',   park_id: '120011' },
+  portola_redwoods_sp: {contract_code: 'CA',   park_id: '120073' },
+  butano_sp:           {contract_code: 'CA',   park_id: '120013' }
 }
+
+PARK_AVAILABILITY_URL = ->(contract_code, park_id) { ->(date) { "http://www.reserveamerica.com/campsiteCalendar.do?page=matrix&calarvdate=#{date.strftime('%m/%d/%Y')}&contractCode=#{contract_code}&parkId=#{park_id}" } }
+
+SPECIFIC_SITE_AVAILABILITY_URL = ->(park_name, contract_code, park_id, site_id) { ->(date) { "http://www.reserveamerica.com/camping/#{park_name}/r/campsiteDetails.do?arvdate=#{date.strftime('%m/%d/%Y')}&contractCode=#{contract_code}&parkId=#{park_id}&siteId=#{site_id}" } }
 
 MAX_WEEKENDS_COUNT = 30
 
@@ -28,6 +31,29 @@ def find_available_weekends(campsite_url, preferred_day: :friday, length_of_stay
     page = Capybara::Node::Simple.new(Nokogiri::HTML(open(campsite_url.(day))))
     availability = {}
     availability[day] = {}
+
+    if page.has_selector?('td#avail1')
+      campsite_name = page.find('.siteTile').text
+      campsite_link = campsite_url.(day).gsub('http://www.reserveamerica.com', '')
+      availability[day][campsite_name] = []
+
+      length_of_stay_in_nights.times do |i|
+        availability[day][campsite_name] << page.find("td#avail#{i + 1}")[:title]
+      end
+
+      if availability[day][campsite_name].all? { |status| status == 'Available' }
+        available_weekends << {
+          date: day.strftime('%a %d %b %Y'),
+          campsite_name: campsite_name,
+          campsite_link: campsite_link,
+          length_of_stay: length_of_stay_in_nights
+        }
+      end
+
+      puts "Checking availability on #{day.strftime('%a %d %b %Y')}. Status: #{availability[day][campsite_name]}"
+
+      next
+    end
     campsite_availability_rows = page.all('#calendar.items tbody tr')
     campsite_availability_rows.each do |row|
       next unless row.has_selector?('td.sn') && row.has_selector?('img') && /tent/.match(row.find('img')[:src])
@@ -78,21 +104,40 @@ end
 
 puts "What park would you like to search availability for?"
 puts "Known parks:\n"
-longest_name_length = CAMPSITE_URLS.keys.max_by(&:size).size
+longest_name_length = PARKS.keys.max_by(&:size).size
 
-CAMPSITE_URLS.keys.each_with_index do |park, index|
+PARKS.keys.each_with_index do |park, index|
   puts <<-PARK_MAP
     #{format_park_name(park).ljust(longest_name_length)} #{index.to_s.rjust(3)}
   PARK_MAP
 end
 
-print ">"
+print "> "
 park_to_search = STDIN.gets.chomp.to_i
-park_key = CAMPSITE_URLS.keys[park_to_search]
-url = CAMPSITE_URLS[park_key]
+park_key = PARKS.keys[park_to_search]
+park_contract_code = PARKS[park_key][:contract_code]
+park_id = PARKS[park_key][:park_id]
+print "\nWould you like to enter a site id to search availability of a specific site at #{format_park_name(park_key)}?(y/n) > "
+specific_site = STDIN.gets.chomp
+if specific_site == 'y' || specific_site == 'Y'
+  print "Enter the site id: "
+  site_id = STDIN.gets.chomp
+end
+if site_id
+  url = SPECIFIC_SITE_AVAILABILITY_URL.(park_key.to_s, park_contract_code, park_id, site_id)
+else
+  url = PARK_AVAILABILITY_URL.(park_contract_code, park_id)
+end
 
 puts "\nSearching for available campsites at #{format_park_name(park_key)} park...\n\n"
 
-find_available_weekends(url).each do |result_hash|
+available_weekends = find_available_weekends(url)
+
+if available_weekends.empty?
+  puts "\n\nNo available weekends to book :("
+  exit
+end
+
+available_weekends.each do |result_hash|
   puts "Campsite #{result_hash[:campsite_name].gsub(/\s/, '').gsub(/Map/,'')} is available on #{result_hash[:date]} for #{result_hash[:length_of_stay]} nights! Book it now at http://reserveamerica.com#{result_hash[:campsite_link]}\n\n"
 end
